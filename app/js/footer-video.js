3d2a66fa592690ade98b92d1bfca3cddb1ea6aaa
RommaH <yatene@ya.ru>
2020-08-20 19:02:06 +0300

    подправил видео слайдер в футере

--- app/js/footer-video.js
+++ app/js/footer-video.js
@@ -1,30 +1,38 @@
 const openVideoSlide = document.querySelectorAll('.site-video');
-const closeVideo = document.querySelector('.video__exit');
-const siteVideoWrapper = document.querySelector('.site-video__wrapper');
+const closeVideo = document.querySelectorAll('.video__exit');
+const siteVideoWrapper = document.querySelectorAll('.site-video__wrapper');
 const siteVideoItem = document.querySelectorAll('.site-video__slide');
 
-let opendVideo;
+
 
 openVideoSlide.forEach(function(item, index){
-	item.addEventListener('click', function(e) {
-		siteVideoWrapper.classList.add('show-video');
-		siteVideoItem[index].classList.add('show-video');
-		opendVideo = index;
+	item.addEventListener('click', function(event) {
+		if(!event.target.closest('.site-video__wrapper')){
+			siteVideoWrapper[index].classList.add('show-video');
+			siteVideoItem[index].play();
+		}
 	});
 })
 
-closeVideo.addEventListener('click', function(e) {
-	siteVideoWrapper.classList.remove('show-video');
-
-		siteVideoItem[opendVideo].classList.remove('show-video');
+closeVideo.forEach(function(item, index){
+	item.addEventListener('click', function() {
+		siteVideoWrapper[index].classList.remove('show-video');
+		siteVideoItem[index].pause();
+	})
 });
-
-
-// openVideoSlide.addEventListener('click', function(e) {
-// 	siteVideoWrapper.classList.add('show-video');
-// 	siteVideoItem.classList.add('show-video');
-// });
-// closeVideo.addEventListener('click', function(e) {
-// 	siteVideoWrapper.classList.remove('show-video');
-// 	siteVideoItem.classList.remove('show-video');
-// });
+siteVideoWrapper.forEach(function(item, index){
+	item.addEventListener('click', function(event) {
+		if (!event.target.closest('.site-video__container')) {
+			siteVideoWrapper[index].classList.remove('show-video');
+			siteVideoItem[index].pause();
+		}
+	})
+});
+window.addEventListener('keydown', function() {
+	if (event.code == 'Escape') {
+		siteVideoWrapper.forEach(function(item, index) {
+			item.classList.remove('show-video');
+			siteVideoItem[index].pause();
+		});
+	}
+});
